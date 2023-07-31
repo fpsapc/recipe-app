e38bd4be14829a80b3d5a5110a11f944c6d58b6d
christianonoh <onohchibyk@gmail.com>
2023-07-31 19:09:53 +0100

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  devise_for :users
+  root to: "users#index"
+  resources :users
   # Defines the root path route ("/")
   # root "articles#index"
 end
