77d98cc99f7cbbe4d510806b2e89d1a0dcfc8873
christianonoh <onohchibyk@gmail.com>
2023-08-01 10:49:53 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: "users#index"
   resources :users
   resources :foods
+  resources :recipes
   # Defines the root path route ("/")
   # root "articles#index"
 end
