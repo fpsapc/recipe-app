3e79f73a00b0f98d5e1e9e620f566e77aa0d9006
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-02 03:57:09 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "users#index"
-  resources :users
+  get '/recipes/missing_food', to: 'recipes#missing_food', as: 'missing_food'
+  get '/public_recipes' => 'recipes#public_recipes'
+  resources :users do
+    get :general_food_list, on: :member
+  end
   resources :foods
   resources :recipes
   resources :recipe_foods, only: [:new, :create, :index]
-  get '/public_recipes' => 'recipes#public_recipes'
 
   # Defines the root path route ("/")
   # root "articles#index"
