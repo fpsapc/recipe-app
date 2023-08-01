edee0c84b59fd6b1fbb4d3aeaf6364e65111d3cc
christianonoh <onohchibyk@gmail.com>
2023-08-01 12:13:17 +0100

    Modify recipe controller and add time format helper

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,37 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+
+    if @recipe.save
+      flash[:notice] = 'Recipe was successfully created.'
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    flash[:notice] = 'Recipe was successfully deleted.'
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+  end
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,12 @@
 module RecipesHelper
+  def format_time(time_in_minutes)
+    hours = time_in_minutes / 60
+    minutes = time_in_minutes % 60
+
+    time_string = ""
+    time_string << "#{hours} hour#{'s' if hours != 1}" if hours > 0
+    time_string << " #{minutes} minute#{'s' if minutes != 1}" if minutes > 0
+
+    time_string
+  end
 end
