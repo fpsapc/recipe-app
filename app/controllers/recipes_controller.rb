32a3692722081748b73cb4813566b5f5f7bb3621
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-02 03:54:42 +0500

    Add missing food to recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,7 +29,11 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
-    @foods = Food.all
+    @foods = if current_user.id == @recipe.user.id
+      RecipeFood.where(recipe_id: @recipe.id).includes(:recipe, :food)
+    else
+      RecipeFood.where(recipe_id: @recipe.id).includes(:food)
+    end
   end
 
   def edit; end
@@ -40,10 +44,31 @@ class RecipesController < ApplicationController
     flash[:notice] = 'Recipe was successfully deleted.'
     redirect_to recipes_path
   end
-
-  private
-
-  def recipe_params
+  
+  def missing_food
+    @user = current_user
+    @general_food_list = @user.foods.group(:name).select('foods.name, SUM(foods.quantity) as total_quantity, SUM(foods.price * foods.quantity) as total_price')
+    
+    @food_used_in_recipes = @user.foods.joins(:recipe_foods).group(:name).select('foods.name, SUM(recipe_foods.quantity) as total_quantity')
+  
+    @missing_food = @general_food_list.map do |general_food|
+      used_food = @food_used_in_recipes.find { |food| food.name == general_food.name }
+      difference_quantity = used_food ? used_food.total_quantity - general_food.total_quantity : general_food.total_quantity
+  
+      # If difference_quantity is less than zero, set it to zero and total_price to zero as well
+      if difference_quantity < 0
+        difference_quantity = 0
+        general_food.total_price = 0
+      else
+        general_food.total_price = general_food.total_price * difference_quantity
+      end
+  
+      OpenStruct.new(name: general_food.name, total_quantity: difference_quantity, total_price: general_food.total_price)
+    end
+  end
+  
+  
+ def recipe_params
     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 
@@ -51,6 +76,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  private
   def authorize_user
     unless @recipe.user == current_user
       redirect_to root_path, alert: 'You are not authorized to perform this action.'
