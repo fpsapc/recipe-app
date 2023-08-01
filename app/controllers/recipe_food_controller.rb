3b5dce841e574d6da54568370826d693a837cb41
christianonoh <onohchibyk@gmail.com>
2023-08-01 21:16:47 +0100

    Work in progress

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -1,5 +1,30 @@
 class RecipeFoodController < ApplicationController
-  def index
-    @recipe_foods = RecipeFood.all
+  # def index
+  #   @recipe_foods = RecipeFood.all
+  # end
+
+  def new
+    @recipe_food = RecipeFood.new
+    @foods = []
+    @ids = RecipeFood.where(recipe_id: params[:recipe_id]).pluck(:food_id)
+    Food.where(user: current_user).each do |f|
+      @foods << [f.name, f.id] unless @ids.include?(f.id)
+    end
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @recipe_food = RecipeFood.new(recipe_food_params.merge(recipe_id: @recipe.id))
+    if @recipe_food.save
+      flash[:notice] = 'Food created sucessfully.'
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  private
+  def recipe_food_params
+    params.require(:recipe_food).permit(:food_id, :quantity)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "users#index"
-  resources :users
+  # resources :users
   resources :foods
-  resources :recipes
-  resources :recipe_food
+  resources :recipes do
+    resources :recipe_foods, only: %i[new create edit update destroy]
+  end
   get '/public_recipes' => 'recipes#public_recipes'
 
   # Defines the root path route ("/")
