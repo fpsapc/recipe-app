c4bbb9c867e5fe37af4ac05d4ecd2da4130129eb
christianonoh <onohchibyk@gmail.com>
2023-08-01 12:48:20 +0100

    Fix linter errors

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -3,9 +3,9 @@ module RecipesHelper
     hours = time_in_minutes / 60
     minutes = time_in_minutes % 60
 
-    time_string = ""
-    time_string << "#{hours} hour#{'s' if hours != 1}" if hours > 0
-    time_string << " #{minutes} minute#{'s' if minutes != 1}" if minutes > 0
+    time_string = ''
+    time_string << "#{hours} hour#{'s' if hours != 1}" if hours.positive?
+    time_string << " #{minutes} minute#{'s' if minutes != 1}" if minutes.positive?
 
     time_string
   end
