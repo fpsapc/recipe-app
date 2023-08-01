3dfa2466460c283f003c1a1f77712eb03cf313d2
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-01 21:14:56 +0500

    Remove validations form recipe_food model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,8 +1,4 @@
 class RecipeFood < ApplicationRecord
     belongs_to :food
     belongs_to :recipe
-
-    validates :food_id, presence: true
-    validates :recipe_id, presence: true
-    validates :quantity, presence: true
 end
