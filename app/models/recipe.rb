a090c397e17252ce9cae6037d1366fafdaae0065
christianonoh <onohchibyk@gmail.com>
2023-08-03 00:48:18 +0100

    Add validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,11 @@
 class Recipe < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id', optional: true
 
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
+
+  validates :name, :preparation_time, :cooking_time, :description, presence: true
+  validates :preparation_time, :cooking_time, comparison: { greater_than_or_equal_to: 0 }
 
   def total_price
     foods.sum(:price)
