579db42f722d1f6f3b0a839720fd69f9ac279fe7
christianonoh <onohchibyk@gmail.com>
2023-08-03 02:48:38 +0100

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,22 +1,19 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     return unless user
 
-    if user
-      # Abilities for all users
-      can %i[read create], Food
-      can %i[read create], Recipe
-      can %i[read create], RecipeFood
+    return unless user
 
-      # Abilities for the user's own records
-      can %i[update destroy], Food, user_id: user.id
-      can %i[update destroy], Recipe, user_id: user.id
-      can %i[update destroy], RecipeFood, user_id: user.id
-    end
+    # Abilities for all users
+    can %i[read create], Food
+    can %i[read create], Recipe
+    can %i[read create], RecipeFood
+
+    # Abilities for the user's own records
+    can %i[update destroy], Food, user_id: user.id
+    can %i[update destroy], Recipe, user_id: user.id
+    can %i[update destroy], RecipeFood, user_id: user.id
   end
 end
-
