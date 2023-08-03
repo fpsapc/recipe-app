d10fe60ca032660b7a29d0cb4e900b8eb514bec2
christianonoh <onohchibyk@gmail.com>
2023-08-03 02:23:50 +0100

    Add ability class

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user
+
+    if user
+      # Abilities for all users
+      can %i[read create], Food
+      can %i[read create], Recipe
+      can %i[read create], RecipeFood
+
+      # Abilities for the user's own records
+      can %i[update destroy], Food, user_id: user.id
+      can %i[update destroy], Recipe, user_id: user.id
+      can %i[update destroy], RecipeFood, user_id: user.id
+    end
+  end
+end
+
