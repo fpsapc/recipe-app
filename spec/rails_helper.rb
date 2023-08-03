3694d63f8687b7947e0a932fbd55e2f8e328718b
christianonoh <onohchibyk@gmail.com>
2023-08-03 18:26:31 +0100

    Fix linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,14 +32,14 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{Rails.root}/spec/fixtures"
- # Include Devise test helpers for request specs (feature specs)
- config.include Devise::Test::IntegrationHelpers, type: :request
+  # Include Devise test helpers for request specs (feature specs)
+  config.include Devise::Test::IntegrationHelpers, type: :request
 
- # Include Devise test helpers for controller specs
- config.include Devise::Test::ControllerHelpers, type: :controller
+  # Include Devise test helpers for controller specs
+  config.include Devise::Test::ControllerHelpers, type: :controller
 
- # Include Devise test helpers for view specs
-config.include Devise::Test::ControllerHelpers, type: :view
+  # Include Devise test helpers for view specs
+  config.include Devise::Test::ControllerHelpers, type: :view
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
